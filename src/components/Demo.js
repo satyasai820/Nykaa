d54c470fb596f7e0e24709cea91549520cdfb65c
unknown <satyasai@actimize.in>
2024-03-17 01:17:47 +0530

    14th commit

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -20,9 +20,6 @@ const Demo = () => {
     }
      const handleData1 = () => {
         console.log('yeah clicked')
-    //    return(
-    //     <Typography>Hi Meena</Typography>
-    //    ); 
     setDisplayData("6ruyiuyiu  ypiyp [yro oirn-[rr oierewoiry ey riweyrerpr u[ro6ftgweyhru'0ep9ireorjeworn  roywherew rruwe rwer8r0ywer werwe[0irwe[0 rwe[r r uwr ew")
         
     }
@@ -31,13 +28,13 @@ const Demo = () => {
         <>
         
         <Grid container sx={{boder:'1px solid red', justifyContent:'center'}}>
-        <Grid sx={{width:'70%', border:'3px solid yellow', display:'flex' }}>
-        <Grid sx={{width:'30%', border:'1px solid green'}}>
-        <Typography onClick={handleData} sx={{border:'1px solid red'}}>Hello</Typography>
+        <Grid sx={{width:'60%', border:'3px solid yellow', display:'flex' }}>
+        <Grid sx={{width:'25%', border:'3px solid green'}}>
+        <Typography onClick={handleData} sx={{border:'1px solid red'}}>My Profile</Typography>
         <Typography onClick={handleData1}>Hello</Typography>
         <Typography>Hello</Typography>
         </Grid>
-        <Grid sx={{width:'70%', border:'1px solid red'}}>
+        <Grid sx={{width:'75%', border:'2px solid red'}}>
             {displayData}
         </Grid>
         </Grid>
--- src/components/ProfileTabOneComponent.js
+++ src/components/ProfileTabOneComponent.js
@@ -1,15 +1,66 @@
-import { Grid,Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 import React from "react";
+import { Icon } from "@iconify/react";
 
 const ProfileTabOneComponent = () => {
-    return(
+    return (
         <>
-        <Grid container sx={{width:'100%',border:'1px solid red'}} >
-         <Grid sx={{width:'95%', border:'2px solid green'}}>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0bv8EL0hhvogzv_NSjDSxkVVs_qAkOS0ZdwEGj0IJcj5QgSkJ" alt="" style={{maxWidth:'100%', height:'auto'}} />
-            <Typography>wssssssssssss</Typography>
-         </Grid>
-        </Grid>
+            <Grid container sx={{ width: '100%', justifyContent: 'center', }} >
+                <Grid container sx={{ width: '95%',  justifyContent: 'center', backgroundImage: 'url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxobxDRYCyPaQG9RboiwzlXF4bu2Mv3Z3vT2XmGrpqaiCpvyqX")' }}>
+                    <Grid sx={{ width: '60%', border: '1px solid black', marginTop: '20px', backgroundColor: '#FFFFFF', display: 'flex', marginBottom: '60px' }}>
+                        <Grid>
+                            <img src="https://www.nykaa.com/assets/mobile/images/my_profile/default_avatar.svg" style={{ width: '120px', margin: '10px 0px 0px 10px' }} alt="" />
+
+                        </Grid>
+                        <Grid sx={{ margin: '40px 0px 0px 15px' }}>
+                            <Typography sx={{ fontWeight: 'bold' }}>Satya Sai</Typography>
+                            <Typography sx={{ fontSize: '13px', color: 'grey', fontWeight: 550 }}>Email: <span style={{ fontWeight: 300 }} >satyaroyals</span></Typography>
+                            <Typography sx={{ fontSize: '13px', color: 'grey', fontWeight: 550 }}>Date of Birth: <span style={{ fontWeight: 300 }} >dd/mm/yyyy</span></Typography>
+                        </Grid>
+                        <Grid sx={{ marginLeft: 'auto' }}>
+                            <Typography sx={{ fontSize: '11px', color: '#000000', textAlign: 'end', padding: '10px 5px' }}> <Icon icon="fluent:edit-48-regular" width="12" height="12" /> Edit</Typography>
+                            <Typography sx={{ fontSize: '11px', color: 'green', padding: '30px 8px 0px 0px', }}>Verified</Typography>
+                        </Grid>
+
+                    </Grid>
+
+                </Grid>
+                <Grid container sx={{ width: '95%', borderBottom: '1px solid lightgrey', backgroundColor: '#FFFFFF', paddingTop: '70px',paddingBottom:'40px',boxShadow:'0px 10px 15px rgba(0, 0, 0, 0.2)'  }}>
+                    <Grid sx={{ width: '50%',  }}>
+                        <Grid container sx={{ alignItems: 'center', marginLeft: '20px' }}>
+                            <Icon icon="ion:home-sharp" width="20" height="20" />
+                            <Typography fontFamily="Inter, Roboto, Arial, sans-serif" sx={{ fontSize: '14px', fontWeight: 550, marginLeft: '5px', color:'#262B2F' }}>  My ADDRESS</Typography>
+                        </Grid>
+                        <Grid sx={{}}>
+                            <Box
+                                sx={{
+                                    display: 'flex',
+                                    flexWrap: 'wrap',
+                                    '& > :not(style)': {
+                                        m: 1,
+                                        width: 268,
+                                        height: 158,
+                                    },
+                                }}
+                            >
+
+                                <Paper elevation={3} >
+                                    <Typography sx={{fontSize:'14px', margin:'10px 0px 0px 15px'}} >No Address Found</Typography>
+                                </Paper>
+                            </Box>
+                        </Grid>
+                    </Grid>
+                    <Grid  sx={{ width: '50%',  }}>
+                        <Grid container sx={{ alignItems:'center', color:'grey', justifyContent:'end', }}>
+                        <Icon icon="fluent:edit-48-regular" width="18" height="18" /> 
+                        <Typography sx={{fontSize:'12px', paddingRight:'20px'}}>ADD NEW ADDRESS</Typography>
+                        </Grid>
+                    </Grid>
+
+                </Grid>
+            </Grid>
         </>
     );
 }
